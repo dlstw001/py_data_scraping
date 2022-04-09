7e9bad9fe173794e6718c14130c1e8a021115d69
dlstw001 <dlstw001@gmail.com>
2022-04-09 20:44:56 +0800

    finished get link function

--- script.py
+++ script.py
@@ -2,18 +2,34 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 
+
 # Setting
 opts = webdriver.ChromeOptions()
 opts.add_argument("--disable-notifications, --headless")
 s = Service(r'C:\Users\user\Desktop\py_data_scraping\driver\chromedriver.exe')
-website = "https://frontierbv.nl/?lang=en"
-wordlst = ['maritime', 'transportation','public safety', 'M2M and IoT', 'financial','Poynting', 'Mondicon']
+website = "https://www.smartone.com/tc/home/"
 
 # Get HTML
 driver = webdriver.Chrome(service=s, options=opts)
-driver.get(website) # need check the drive element
-html = driver.page_source
-soup = BeautifulSoup(html, 'lxml')
-test = soup.find('a', class_='xQ_iF')
-print(test)
+driver.get(website)
+results = []
+content = driver.page_source
+soup = BeautifulSoup(content, 'lxml')
+driver.quit()
+
+# Check keywords exists
+""" for keyword in keywordslist: """
+
+
+
+
+# Get all the link from the webpage
+for element in soup.findAll('a', href=True):
+    link = str(element.get('href'))
+    check = ['http://', 'https://']
+    if any(x in link for x in check) and link not in results:
+        results.append(link)
+
+
+
 

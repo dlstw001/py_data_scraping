80779de0a1a6d6677aaf1521af7b24d2fec3958a
dlstw001 <dlstw001@gmail.com>
2022-04-10 15:55:21 +0800

    'little adjust'

--- script.py
+++ script.py
@@ -2,17 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 
-
 # Setting
 opts = webdriver.ChromeOptions()
 opts.add_argument("--disable-notifications, --headless")
 s = Service(r'C:\Users\user\Desktop\py_data_scraping\driver\chromedriver.exe')
 website = "https://www.smartone.com/tc/home/"
+link_lst = []
 
 # Get HTML
 driver = webdriver.Chrome(service=s, options=opts)
 driver.get(website)
-results = []
 content = driver.page_source
 soup = BeautifulSoup(content, 'lxml')
 driver.quit()
@@ -20,16 +19,9 @@ driver.quit()
 # Check keywords exists
 """ for keyword in keywordslist: """
 
-
-
-
 # Get all the link from the webpage
 for element in soup.findAll('a', href=True):
     link = str(element.get('href'))
     check = ['http://', 'https://']
-    if any(x in link for x in check) and link not in results:
-        results.append(link)
-
-
-
-
+    if any(x in link for x in check) and link not in link_lst:
+        link_lst.append(link)

360873fc359ed7a92c24b1296a7a2a985fff23ac
User <dlstw001@gmail.com>
2022-04-11 16:51:14 +0800

    finished get link function

--- script.py
+++ script.py
@@ -2,31 +2,48 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import time
+import tldextract
 
 
 # Setting
 opts = webdriver.ChromeOptions()
-opts.add_argument("--disable-notifications")
+opts.add_argument("--disable-notifications, --headless")
 s = Service(r'C:\Users\davidl\Desktop\py_data_scraping\driver\chromedriver.exe')
-link_lst = ["https://www.smartone.com/tc/home/"]
+global_link_lst = ["https://www.smartone.com/tc/home/"]
 driver = webdriver.Chrome(service=s, options=opts)
 
-# Get HTML
-for link in link_lst:
-    driver.get(link)
-    time.sleep(3)
-    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-    time.sleep(2)
-    content = driver.page_source
-    soup = BeautifulSoup(content, 'lxml')
 
-    # Get all the link from the webpage
-    for element in soup.findAll('a', href=True):
-        link = str(element.get('href'))
-        check = ['http://', 'https://']
-        if any(x in link for x in check) and link not in link_lst:
-            print(link)
-            link_lst.append(link)
+# Main Function
+for item in global_link_lst:
+    # Create Local workspace
+    local_link_lst = [item]
+    domain = tldextract.extract(item).domain
+    for link in local_link_lst:
+        driver.get(link)
 
+        # Scroll down until the end
+        time.sleep(3)
+        previous_height = driver.execute_script('return document.body.scrollHeight')
+        while True:
+            driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+            time.sleep(3)
+            new_height = driver.execute_script('return document.body.scrollHeight')
+            if new_height == previous_height:
+                break
+
+        # Make BeautifulSoup
+        content = driver.page_source
+        soup = BeautifulSoup(content, 'lxml')
+
+        # Get all the link from the soup
+        for element in soup.findAll('a', href=True):
+            link = str(element.get('href'))
+            check = tldextract.extract(link).domain
+            if (link.startswith('http://') or link.startswith('https://')) and\
+                    (domain == check) and (link not in local_link_lst):
+                print(link)
+                local_link_lst.append(link)
+
+    # Get all the text from the page
 
 
